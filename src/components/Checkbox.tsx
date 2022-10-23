9ccf2c83b0f503b0435e62fc971e3e91da032119
Anderson Silva Mota <andersonsilv.mota@gmail.com>
2022-10-22 22:31:27 -0300

    Design system react

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,17 @@
+import * as CheckboxPrimitivo from '@radix-ui/react-checkbox'
+import { Check } from 'phosphor-react'
+
+export interface CheckboxProps {}
+
+export function Checkbox({}: CheckboxProps) {
+
+  return (
+    <CheckboxPrimitivo.Root 
+      className='w-6 h-6 p-[2px] bg-gray-800 rounded'
+    >
+      <CheckboxPrimitivo.Indicator asChild>
+        <Check weight='bold' className='h5 w-5 text-cyan-500' />
+      </CheckboxPrimitivo.Indicator>
+    </CheckboxPrimitivo.Root>
+  )
+}
